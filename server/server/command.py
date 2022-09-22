ce825e09628d9b0599017e3f3c56f5636913d70a
Noe <noe.berguin@polymtl.ca>
2022-09-22 10:38:20 -0400

    server-interface for debug

---
+++ server/server/command.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Command(Enum):
+    START_ALGORITHM = 0
+    CONNECT_WATCH = 1
+    
---
+++ server/server/command_handler.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from inspect import _void
+import json
+from lib2to3.pgen2.token import STAR
+import sys
+from typing import Callable, Union
+from command import Command
+from algorithm.NeuroAlgorithmPrediction import NeuroAlgorithmPrediction
+
+
+class Mode(Enum):
+    SERIAL = 0
+    STAR = 1
+
+
+class CommandHandler:
+    def __init__(self):
+        self.current_handler = None
+
+
+    def handle_command(self, command: int, arg: Union[int, dict, str]) -> Union[None, list, int]:
+        if command == Command.START_ALGORITHM.value:
+            Algo = NeuroAlgorithmPrediction()
+            Algo.generate_space(int(arg["dimention"]),int(arg["n_param"]))
+            response = Algo.run()
+            data = { 
+                "data" : json.dumps(response[0]), 
+                "position" : json.dumps(response[1]), 
+                "n_param" :  Algo.n_param,
+                "dimention" : Algo.dimention}
+            return data
+            
+
+        # elif command == Command.NEW_MISSION.value:
+        #     self.database.add_mission(arg)
+
+
+    def release(self, *_) -> None:
+        if self.current_handler != None:
+            self.current_handler.release()
+        sys.exit(0)
+
+

155d09f396b43aa15f7b51f3d45656146bf9c363
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2022-09-24 17:26:54 -0400

    Added bluetooth scanning ability

--- Tablet/NeuralDrive/src/views/Main.js
+++ Tablet/NeuralDrive/src/views/Main.js
@@ -1,6 +1,14 @@
 // React Native Imports
 import React, {useState} from 'react';
-import {Text, TextInput, ScrollView, Button, ToastAndroid, StyleSheet} from 'react-native';
+import {
+  Text,
+  TextInput,
+  ScrollView,
+  SafeAreaView,
+  Button,
+  ToastAndroid,
+  StyleSheet,
+} from 'react-native';
 
 // Component Imports
 // import RoundedButton from '../components/RoundedButton.js';
@@ -8,31 +16,39 @@ import {Text, TextInput, ScrollView, Button, ToastAndroid, StyleSheet} from 'rea
 import Buttons from '../components/RoundedButton.js';
 import * as OtherButtons from '../components/OtherButtons.js';
 import * as Inputs from '../components/Inputs.js';
+import WatchManager from '../components/WatchManager.js';
 
 // Style Imports
 import * as ColorTheme from '../styles/Colors';
 
 const Main = () => {
   return (
-    <ScrollView style={styles.viewContainer}>
-      <Buttons.RoundedButton title="Record" onPress={()=>blank()} bgColor={ColorTheme.Fruity.First}></Buttons.RoundedButton>
-      <Inputs.RegInput />
-      <Inputs.Round />
-      <Inputs.RoundGrey />
-      <OtherButtons.TO>
-        <Text> Okay </Text>
-      </OtherButtons.TO>
-      <OtherButtons.Basic>
-        <Text> Text </Text>
-      </OtherButtons.Basic>
-      <Buttons.Test title="Whut" onPress={()=>blank()} bgColor={ColorTheme.Fruity.First}></Buttons.Test>
-    </ScrollView>
+    <SafeAreaView style={styles.viewContainer}>
+      <WatchManager />
+      <ScrollView>
+        <Buttons.RoundedButton
+          title="Record"
+          onPress={() => blank()}
+          bgColor={ColorTheme.Fruity.First}></Buttons.RoundedButton>
+        <Inputs.RegInput />
+        <Inputs.Round />
+        <Inputs.RoundGrey />
+        <OtherButtons.TO>
+          <Text> Okay </Text>
+        </OtherButtons.TO>
+        <OtherButtons.Basic>
+          <Text> Text </Text>
+        </OtherButtons.Basic>
+        <Buttons.Test
+          title="Whut"
+          onPress={() => blank()}
+          bgColor={ColorTheme.Fruity.First}></Buttons.Test>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
-function blank(){
-
-};
+function blank() {}
 
 const styles = StyleSheet.create({
   viewContainer: {

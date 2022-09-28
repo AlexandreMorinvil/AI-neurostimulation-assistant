367ee7cb46acd1990dd302d6d99a2bce0c7d051c
Noe99 <noe.berguin@polymtl.ca>
2022-09-28 15:43:21 -0400

    add chart librairie

--- Tablet/NeuralDrive/src/views/ServerTesting.js
+++ Tablet/NeuralDrive/src/views/ServerTesting.js
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';
-import {Text, TextInput, ScrollView, Button, ToastAndroid, StyleSheet} from 'react-native';
-// import * as Inputs from '../components/Inputs.js';
 
+import styled from 'styled-components';
+import {
+  Text,
+  TextInput,
+  ScrollView,
+  Button,
+  ToastAndroid,
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+// import * as Inputs from '../components/Inputs.js';
+//import Chart from 'react-native-chartjs';
 const ServerTesting = () => {
   const [text, setText, serverResponse] = useState('');
 
   return (
     // <ScrollView style={{padding: 10}}>
-    <ScrollView style={styles.viewContainer}>
-      <TextInput
+    <View style={styles.viewContainer}>
+      {/* <TextInput
         style={{height: 40}}
         placeholder="Server Input"
         onChangeText={text => setText(text)}
@@ -22,7 +41,7 @@ const ServerTesting = () => {
         }}
       />
       <Text style={{padding: 10, fontSize: 16}}>
-        Message to be sent: {text}
+        Message to TESTTTTTTTTT be sent: {text}
       </Text>
       <Text style={{padding: 10, fontSize: 16}}> Server Response: </Text>
       <Text
@@ -33,15 +52,119 @@ const ServerTesting = () => {
           color: '#eee',
         }}>
         blabla... {serverResponse}
-      </Text>
-    </ScrollView>
+      </Text> */}
+      <View style={styles.box}>
+        <Text style={styles.boxTitle}>Input</Text>
+        <View style={styles.inputBox}>
+          <Text>Dimensions</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="4"
+            onChangeText={text => setText(text)}
+            defaultValue={text}
+          />
+        </View>
+        <View style={styles.inputBox}>
+          <Text>Number of parameters</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="2"
+            onChangeText={text => setText(text)}
+            defaultValue={text}
+          />
+        </View>
+        <Button
+          title="send to server"
+          onPress={() => {
+            // Call Server Request Here @Noe
+            ToastAndroid.show('Sending server request...', ToastAndroid.SHORT);
+          }}
+        />
+      </View>
+      <View style={styles.boxChart}>
+        <Text style={styles.boxTitle}>Patient data</Text>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width * 0.49} // from react-native
+          height={Dimensions.get('window').height * 0.9}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   viewContainer: {
-    flex: 1,
-    backgroundColor: '#444',
+    width: '100%',
+    height: '100%',
+    padding: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  inputBox: {
+    width: '40%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  box: {
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'grey',
+    padding: 5,
+  },
+  boxChart: {
+    width: '50%',
+    height: '100%',
+    backgroundColor: 'cyan',
+    padding: 5,
+  },
+  boxTitle: {
+    width: '100%',
+    alignItems: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 5,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
     padding: 10,
   },
 });

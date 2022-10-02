fba93b18048e86e4ef63a690e0b7a9a0f7809ac8
Noe99 <noe.berguin@polymtl.ca>
2022-10-01 22:21:15 -0400

    start add canvas

--- Tablet/NeuralDrive/src/views/ServerTesting.js
+++ Tablet/NeuralDrive/src/views/ServerTesting.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-
+import Chart from '../components/Chart';
+import Canva from '../components/Canvas';
+import {send_request} from '../class/http';
 import styled from 'styled-components';
 import {
   Text,
@@ -19,111 +21,51 @@ import {
   ContributionGraph,
   StackedBarChart,
 } from 'react-native-chart-kit';
-// import * as Inputs from '../components/Inputs.js';
-//import Chart from 'react-native-chartjs';
+
+server_url = 'http://10.0.2.2:5000/packet';
+
 const ServerTesting = () => {
   const [text, setText, serverResponse] = useState('');
 
   return (
     // <ScrollView style={{padding: 10}}>
     <View style={styles.viewContainer}>
-      {/* <TextInput
-        style={{height: 40}}
-        placeholder="Server Input"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
-      />
-      <Button
-        title="send to server"
-        onPress={() => {
-          // Call Server Request Here @Noe
-          ToastAndroid.show('Sending server request...', ToastAndroid.SHORT);
-        }}
-      />
-      <Text style={{padding: 10, fontSize: 16}}>
-        Message to TESTTTTTTTTT be sent: {text}
-      </Text>
-      <Text style={{padding: 10, fontSize: 16}}> Server Response: </Text>
-      <Text
-        style={{
-          padding: 10,
-          height: 1000,
-          backgroundColor: '#222',
-          color: '#eee',
-        }}>
-        blabla... {serverResponse}
-      </Text> */}
-      <View style={styles.box}>
-        <Text style={styles.boxTitle}>Input</Text>
-        <View style={styles.inputBox}>
-          <Text>Dimensions</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="4"
-            onChangeText={text => setText(text)}
-            defaultValue={text}
+      <View style={styles.upperbox}>
+        <View style={styles.box}>
+          <Text style={styles.boxTitle}>Input</Text>
+          <View style={styles.inputBox}>
+            <Text>Dimensions</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="4"
+              onChangeText={text => setText(text)}
+              defaultValue={text}
+            />
+          </View>
+          <View style={styles.inputBox}>
+            <Text>Number of parameters</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="2"
+              onChangeText={text => setText(text)}
+              defaultValue={text}
+            />
+          </View>
+          <Button
+            title="send to server"
+            onPress={() => {
+              // Call Server Request Here @Noe
+              send_request(server_url);
+            }}
           />
         </View>
-        <View style={styles.inputBox}>
-          <Text>Number of parameters</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="2"
-            onChangeText={text => setText(text)}
-            defaultValue={text}
-          />
+        <View style={styles.box}>
+          <Canva></Canva>
         </View>
-        <Button
-          title="send to server"
-          onPress={() => {
-            // Call Server Request Here @Noe
-            ToastAndroid.show('Sending server request...', ToastAndroid.SHORT);
-          }}
-        />
       </View>
       <View style={styles.boxChart}>
         <Text style={styles.boxTitle}>Patient data</Text>
-        <LineChart
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-            datasets: [
-              {
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                ],
-              },
-            ],
-          }}
-          width={Dimensions.get('window').width * 0.49} // from react-native
-          height={Dimensions.get('window').height * 0.9}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: '6',
-              strokeWidth: '2',
-              stroke: '#ffa726',
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
+        <Chart></Chart>
       </View>
     </View>
   );
@@ -137,13 +79,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
+  upperbox: {
+    width: '50%',
+    height: '100%',
+    flexDirection: 'column',
+  },
   inputBox: {
     width: '40%',
     flexDirection: 'row',
     alignItems: 'center',
   },
   box: {
-    width: '50%',
+    width: '100%',
     height: '50%',
     backgroundColor: 'grey',
     padding: 5,
@@ -151,7 +98,7 @@ const styles = StyleSheet.create({
   boxChart: {
     width: '50%',
     height: '100%',
-    backgroundColor: 'cyan',
+    backgroundColor: 'grey',
     padding: 5,
   },
   boxTitle: {

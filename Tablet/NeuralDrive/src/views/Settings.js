8cacb5a2b4369c295388e2877c3d713e6c45fe9b
Tristan Philis <tristan.philis@polymtl.ca>
2022-10-20 18:19:17 -0400

    Show IP address on the setting page of the tablet
    Signed-off-by: tristan Philis

--- Tablet/NeuralDrive/src/views/Settings.js
+++ Tablet/NeuralDrive/src/views/Settings.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {
   Text,
   TextInput,
@@ -6,17 +6,86 @@ import {
   Button,
   ToastAndroid,
   StyleSheet,
+  View,
+  Switch,
 } from 'react-native';
 import * as Inputs from '../components/Inputs.js';
+import Buttons from '../components/Buttons.js';
+import MainModules from '../components/MainModules.js';
+import {Picker} from '@react-native-picker/picker';
+import NetInfo, { NetInfoStateType } from '@react-native-community/netinfo';
+// Style Imports
+import * as ColorTheme from '../styles/Colors';
+import { faTruckMoving } from '@fortawesome/free-solid-svg-icons';
 
 const SettingsScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => {setIsEnabled(previousState => !previousState)}
+  const [selectedValue, setSelectedValue] = useState("none");
+  const isConnectedToBluetooth = false;
+
+  const [netInfo, setNetinfo] = useState('');
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setNetinfo(
+        `Connection type: ${state.type}
+        Is connected?: ${state.isConnected}
+        IP Address: ${state.details.ipAddress}`,
+      );
+    });
+    return () =>{
+      unsubscribe();
+    };
+  }, []);
+
+  const getNetInfo = () => {
+    NetInfo.fetch().then((state) => {
+      alert(
+        `Connection type: ${state.type}
+        Is connected?: ${state.isConnected}
+        IP Address: ${state.details.ipAddress}`,
+      );
+    });
+  };
+
   return (
-    <ScrollView style={styles.viewContainer}>
-      <Text style={{color: '#fff'}}> Settings </Text>
-    </ScrollView>
+    <View style={styles.viewContainer}
+      alignItems='center'>
+        <MainModules.Box height='100%' width='100%' bgColor='#222' jc='flex-start' alignItems='center'>
+          <Text style={{ color: "#fff" }}> Neurodrive data app </Text>
+          <MainModules.Box height={'250px'} width={'100%'} bgColor={'#555'} flexDirection="row">
+            <MainModules.FlexContainer bgColor={'#555'} flexDirection="column">
+              <MainModules.WatchIPInputModule
+                flex={1}
+                alignItems={'center'}
+                bgColor={'#555'}
+                ipAddress={netInfo}
+              />
+            </MainModules.FlexContainer>
+            <MainModules.FlexContainer bgColor={'#555'} flexDirection="column">
+              <MainModules.ServerURLInputModule
+                flex={1}
+                alignItems={'flex-start'}
+                bgColor={'#555'}
+              />
+              <Switch 
+              trackColor ={{ false: 'red', true: 'green'}} 
+              thumbColor={isEnabled? 'white' : 'white'} 
+              onValueChange={toggleSwitch} 
+              value={isEnabled}
+              />
+            </MainModules.FlexContainer>
+          </MainModules.Box>
+          <Buttons.RoundedButton
+          title="Main page"
+          onPress={() => blank()}
+          bgColor={ColorTheme.Fruity.First}
+          container={bottom = 10} />
+        </MainModules.Box>
+    </View>
   );
 };
-//
+
 
 const styles = StyleSheet.create({
   viewContainer: {
@@ -26,4 +95,7 @@ const styles = StyleSheet.create({
   },
 });
 
+function blank() {
+  // To be completed
+}
 export default SettingsScreen;
